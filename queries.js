266cf2d577daf042d28b4d128e813c66c2afcaba
kapalotkofi <agyepo813@morrisville.edu>
2026-05-19 07:15:38 -0400

    Update database connection to use DATABASE_URL for Render

--- queries.js
+++ queries.js
@@ -4,12 +4,9 @@ import pg from 'pg'
 const { Pool } = pg
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
-})
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+  })
 
 const getTasks = async (request, response) => {
   try {
